e17f5c73ad01f3792394885d3410a62aa0e69abe
Lucas Godoy <lucas.godoy@sjc.sp.gov.br>
2025-07-16 10:10:11 -0300

    subindo

---
+++ src/main/java/br/com/fiap/ms_error_tracker/MsErrorTrackerApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.ms_error_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsErrorTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsErrorTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/ms_error_tracker/MsErrorTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.ms_error_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsErrorTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
